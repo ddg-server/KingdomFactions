cea7e46abd88338dbcd95a02e0d29e9257628fb3
Duckelekuuk <duckelekuuk@gmail.com>
2020-10-01 14:56:26 +0200

    More cleanup

--- src/nl/dusdavidgames/kingdomfactions/modules/faction/FactionMember.java
+++ src/nl/dusdavidgames/kingdomfactions/modules/faction/FactionMember.java
@@ -9,47 +9,47 @@ import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
+@Getter
 public class FactionMember {
 
-	
-	public FactionMember(Faction f, UUID uuid, String name, FactionRank rank) {
-		this.uuid = uuid;
-		this.name = name;
-		this.rank = rank;
-		this.faction = f;
-	}
-	public boolean isOnline() {
-		return Bukkit.getPlayer(uuid) != null;
-	}
-	
-	public Player toBukkitPlayer() {
-		return Bukkit.getPlayer(uuid);
-	}
-	
-	
-	private @Getter UUID uuid;
-	private @Getter String name;
-	private @Getter Faction faction;
-	private  @Getter FactionRank rank;
-	
-	public IPlayerBase toPlayer() {
-	        try {
-				return PlayerModule.getInstance().getPlayerBase(uuid);
-			} catch (UnkownPlayerException e) {
-		
-			}
-			return null;
-	}
-	
-	public void setRank(FactionRank rank) {
-		this.rank = rank;
+    private UUID uuid;
+    private String name;
+    private Faction faction;
+    private FactionRank rank;
 
-	}
-	
-	public void updateName() {
-	IPlayerBase base = toPlayer();
-	if(!base.getName().equalsIgnoreCase(this.name)) {
-		this.name = base.getName();
-	}
-	}
+    public FactionMember(Faction faction, UUID uuid, String name, FactionRank rank) {
+        this.uuid = uuid;
+        this.name = name;
+        this.rank = rank;
+        this.faction = faction;
+    }
+
+    public boolean isOnline() {
+        return Bukkit.getPlayer(uuid) != null;
+    }
+
+    public Player toBukkitPlayer() {
+        return Bukkit.getPlayer(uuid);
+    }
+
+    public IPlayerBase toPlayer() {
+        try {
+            return PlayerModule.getInstance().getPlayerBase(uuid);
+        } catch (UnkownPlayerException e) {
+
+        }
+        return null;
+    }
+
+    public void setRank(FactionRank rank) {
+        this.rank = rank;
+
+    }
+
+    public void updateName() {
+        IPlayerBase base = toPlayer();
+        if (!base.getName().equalsIgnoreCase(this.name)) {
+            this.name = base.getName();
+        }
+    }
 }
